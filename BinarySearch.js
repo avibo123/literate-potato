f74bd7b6c1f9e95494961e43faf0cda257951046
avibo123 <46800038+avibo123@users.noreply.github.com>
2019-08-18 11:42:56 -0700

    Update BinarySearch.js

--- BinarySearch.js
+++ BinarySearch.js
@@ -1,12 +1,13 @@
 /* Returns either the index of the location in the array,
   or -1 if the array did not contain the targetValue */
 var doSearch = function(array, targetValue) {
-	var min = 0;
-	var max = array.length - 1;
+    var min = 0;
+    var max = array.length - 1;
     var guess;
     
     while(max >= min) {
         guess = Math.floor((max+min)/2);
+	println("guess: " + guess);    
         if(array[guess] === targetValue){return guess;}
         else if(array[guess] < targetValue){min = guess + 1;}
         else{max = guess - 1;}
