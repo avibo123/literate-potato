4a705f131d49f35333032a28f02cdcfe47a0383b
avibo123 <46800038+avibo123@users.noreply.github.com>
2019-08-18 14:08:17 -0700

    Create Swap.js

---
+++ Swap.js
@@ -0,0 +1,20 @@
+var swap = function(array, firstIndex, secondIndex) {
+    var save = array[firstIndex];
+	array[firstIndex] = array[secondIndex];
+	array[secondIndex] = save;
+};
+
+var testArray = [7, 9, 4];
+swap(testArray, 0, 1);
+
+println(testArray);
+
+Program.assertEqual(testArray, [9, 7, 4]);
+
+swap(testArray, 1, 2);
+println(testArray);
+Program.assertEqual(testArray, [9, 4, 7]);
+
+swap(testArray, 2, 0);
+println(testArray);
+Program.assertEqual(testArray, [7, 4, 9]);
