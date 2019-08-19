b00c28155de4f5ecf69b20a12e7063440055d6ad
avibo123 <46800038+avibo123@users.noreply.github.com>
2019-08-19 16:19:15 -0700

    Create Partition.js

---
+++ Partition.js
@@ -0,0 +1,40 @@
+// Swaps two items in an array, changing the original array
+var swap = function(array, firstIndex, secondIndex) {
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+};
+
+var partition = function(array, p, r) {
+    // Compare array[j] with array[r], for j = p, p+1,...r-1
+    // maintaining that:
+    //  array[p..q-1] are values known to be <= to array[r]
+    //  array[q..j-1] are values known to be > array[r]
+    //  array[j..r-1] haven't been compared with array[r],
+    //   increment q, and increment j. 
+    // Once all elements in array[p..r-1]
+    //  have been compared with array[r],
+    //  swap array[r] with array[q], and return q.
+    var  q= p;
+    for (var j=p; j<=r-1; j++) {
+        if (array[j] <= array[r]) {
+            // If array[j] <= array[r], swap array[j] with array[q]
+            swap(array, j, q);
+            q++;
+        }
+    }
+    swap(array, r, q);
+    return q;
+};
+
+var array = [9, 7, 5, 11, 12, 2, 14, 3, 10, 4, 6];
+var q = partition(array, 0, array.length-1);
+println("Array after partitioning: " + array);
+Program.assertEqual(q, 4);
+Program.assertEqual(array, [5, 2, 3, 4, 6, 7, 14, 9, 10, 11, 12]);
+
+var array2 = [-1, 7, 5, 11, 12, 2, 14, 3, 10, 4, 6];
+var q1 = partition(array2, 0, array2.length-1);
+println("Array after partitioning: " + array2);
+Program.assertEqual(q1, 5);
+Program.assertEqual(array2, [-1, 5, 2, 3, 4, 6, 14, 11, 10, 12, 7]);
